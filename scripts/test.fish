5fa4508ed5fa7191d94542ec5688051a8dd7f565
Hery Fitiavana <105294881+heryfitiavana22@users.noreply.github.com>
2025-04-29 21:02:06 +0300

    test: automatic run scripts/test.* (#105)
    * test: automatic run scripts/test.*
    
    * test: update infisical image to latest version and add test scripts for vault
    
    * ci: test workflow by adding run test vault
    
    * refact: move to test folder and print bash, zsh, fish versioin

--- scripts/test.fish
+++ scripts/test.fish
@@ -12,5 +12,18 @@ echo "test=$TEST"
 eval "$(cargo run -- off)"
 echo "test=$TEST"
 
-preexec_lade
-precmd_lade
+type -q preexec_lade
+if test $status -eq 0
+    echo "preexec_lade should not exist after lade off"
+    exit 1
+else
+    echo "preexec_lade correctly removed"
+end
+
+type -q precmd_lade
+if test $status -eq 0
+    echo "precmd_lade should not exist after lade off"
+    exit 1
+else
+    echo "precmd_lade correctly removed"
+end
