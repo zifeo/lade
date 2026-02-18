7cdfd338a2c1beb82741749fca6c3352b903d0fa
Teo Stocco <teo@zifeo.com>
2026-02-17 23:24:44 -0800

    fix: gh

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,7 +2,11 @@ use assert_cmd::Command;
 
 pub fn lade(home: &std::path::Path) -> Command {
     let mut cmd = Command::cargo_bin("lade").unwrap();
-    cmd.env("LADE_SHELL", "bash").env("HOME", home);
+    cmd.env("LADE_SHELL", "bash")
+        .env("HOME", home)
+        .env("XDG_CONFIG_HOME", home.join(".config"))
+        .env("XDG_DATA_HOME", home.join(".local/share"))
+        .env("XDG_CACHE_HOME", home.join(".cache"));
     cmd
 }
 
